e8b9af10f4d7099c92b36e376eb9141ae463c3aa
lumamontes <lumagoesmontes@gmail.com>
2023-12-31 05:40:18 -0300

    fix: base url for services

--- src/controllers/scheduleController.js
+++ src/controllers/scheduleController.js
@@ -13,29 +13,33 @@ exports.getSchedule = async (req, res) => {
   } = req.query;
 
   let results;
-  switch (league) {
-    case "ncaaw":
-      results = await getScheduleNcaaw({
-        year,
-        month,
-        day,
-        groups,
-        seasontype,
-        limit,
-      });
-      break;
-    case "wnba":
-      results = await getScheduleWnba({
-        year,
-        month,
-        day,
-        groups,
-        seasontype,
-        limit,
-      });
-      break;
-    default:
-      throw new Error("Invalid league");
+  try {
+    switch (league) {
+      case "ncaaw":
+        results = await getScheduleNcaaw({
+          year,
+          month,
+          day,
+          groups,
+          seasontype,
+          limit,
+        });
+        break;
+      case "wnba":
+        results = await getScheduleWnba({
+          year,
+          month,
+          day,
+          groups,
+          seasontype,
+          limit,
+        });
+        break;
+      default:
+        throw new Error("Invalid league");
+    }
+    res.json(results);
+  } catch (error) {
+    res.status(500).send(error.message);
   }
-  res.json(results);
 };
